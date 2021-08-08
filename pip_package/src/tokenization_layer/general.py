e00df47ae16c3b5f05a6cbea4815c38c7ba403ad
martinm07 <martin.github07@gmail.com>
2021-08-08 15:13:29 +0100

    Make package modular

---
+++ pip_package/src/tokenization_layer/general.py
@@ -0,0 +1,4 @@
+import tensorflow as tf # We use so much TensorFlow here we might as well just import it all.
+from numpy.random import choice
+from keras.initializers import Initializer
+from keras.layers import Layer
